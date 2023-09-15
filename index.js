afcf664f34360ad8a31adea27fbb23c871a89b81
cestchrissii <candagraphicdesigns@gmail.com>
2023-09-15 16:05:00 -0400

    Updated website content

--- index.js
+++ index.js
@@ -9,7 +9,12 @@ document.addEventListener("DOMContentLoaded", function () {
       delay: 5000, // Change slide every 5 seconds
     },
   });
+  const btnNav = document.querySelector(".btn-mobile-nav");
+  const header = document.querySelector(".header");
 
+  btnNav.addEventListener("click", function () {
+    header.classList.toggle("nav-open");
+  });
   // Subscribe button click event
   const subscribeButton = document.querySelector(".btn--form");
   subscribeButton.addEventListener("click", function (event) {
@@ -275,3 +280,79 @@ document.addEventListener("DOMContentLoaded", function () {
 
 // Initialize the fairy dust effect
 // particlesJS("hero-dust");
+
+///////////////////////////////////////////////////////////////////
+// Smooth Scrolling Animation
+const allLinks = document.querySelector("a:link");
+// console.log(allLinks)
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+    console.log(href);
+
+    //scroll back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    //scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      console.log(sectionEl);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    //close mobile navigation
+    if (link.classList.contains("main-nav-link"))
+      headerEL.classList.toggle("nav-open");
+  });
+});
+
+///////////////////////////////////////////////////////////////////
+//sticky navigation
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+
+    if (!ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // in the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
+
+//////////////////////////////////////////////////////////////////////////
+// Fixing flexbox gap property missing in some Safari versions
+function checkFlexGap() {
+  var flex = document.createElement("div");
+  flex.style.display = "flex";
+  flex.style.flexDirection = "column";
+  flex.style.rowGap = "1px";
+
+  flex.appendChild(document.createElement("div"));
+  flex.appendChild(document.createElement("div"));
+
+  document.body.appendChild(flex);
+  var isSupported = flex.scrollHeight === 1;
+  flex.parentNode.removeChild(flex);
+  console.log(isSupported);
+
+  if (!isSupported) document.body.classList.add("no-flexbox-gap");
+}
+checkFlexGap();
+
+//https://unpkg.com/smoothscroll-polyfill@0.4.4/dist/smoothscroll.min.js
