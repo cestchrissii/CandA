04d9ae8b2d079072b467a471a1053cef5a7052a8
cestchrissii <candagraphicdesigns@gmail.com>
2023-09-28 22:27:42 -0400

    deploy

--- templates.js
+++ templates.js
@@ -743,9 +743,92 @@ document.addEventListener("DOMContentLoaded", function () {
   setInterval(highlightProduct, intervalTime);
 });
 
-var client = ShopifyBuy.buildClient({
-  domain: "thisiscanda.myshopify.com",
-  storefrontAccessToken: "fe68251fab787c99f3e14494d10c403c",
-});
-
-var ui = ShopifyBuy.UI.init(client);
+(function () {
+  var scriptURL =
+    "https://sdks.shopifycdn.com/buy-button/latest/buy-button-storefront.min.js";
+  if (window.ShopifyBuy) {
+    if (window.ShopifyBuy.UI) {
+      ShopifyBuyInit();
+    } else {
+      loadScript();
+    }
+  } else {
+    loadScript();
+  }
+  function loadScript() {
+    var script = document.createElement("script");
+    script.async = true;
+    script.src = scriptURL;
+    (
+      document.getElementsByTagName("head")[0] ||
+      document.getElementsByTagName("body")[0]
+    ).appendChild(script);
+    script.onload = ShopifyBuyInit;
+  }
+  function ShopifyBuyInit() {
+    var client = ShopifyBuy.buildClient({
+      domain: "thisiscanda.myshopify.com",
+      storefrontAccessToken: "381ff4526fc386e823c143fc63829720",
+    });
+    ShopifyBuy.UI.onReady(client).then(function (ui) {
+      ui.createComponent("product", {
+        id: "8126268932409",
+        node: document.getElementById("test-checkout-1"),
+        moneyFormat: "%24%7B%7Bamount%7D%7D",
+        options: {
+          product: {
+            styles: {
+              product: {
+                "@media (min-width: 601px)": {
+                  "max-width": "calc(25% - 20px)",
+                  "margin-left": "20px",
+                  "margin-bottom": "50px",
+                },
+              },
+            },
+            text: {
+              button: "Add to cart",
+            },
+          },
+          productSet: {
+            styles: {
+              products: {
+                "@media (min-width: 601px)": {
+                  "margin-left": "-20px",
+                },
+              },
+            },
+          },
+          modalProduct: {
+            contents: {
+              img: false,
+              imgWithCarousel: true,
+              button: false,
+              buttonWithQuantity: true,
+            },
+            styles: {
+              product: {
+                "@media (min-width: 601px)": {
+                  "max-width": "100%",
+                  "margin-left": "0px",
+                  "margin-bottom": "0px",
+                },
+              },
+            },
+            text: {
+              button: "Add to cart",
+            },
+          },
+          option: {},
+          cart: {
+            text: {
+              total: "Subtotal",
+              button: "Checkout",
+            },
+          },
+          toggle: {},
+        },
+      });
+    });
+  }
+})();
